0c4e30e01446adc1adf0c1be1d4c0b3342e3afb7
MiqueiasBrandaoDev <miqueiasbrandaogyn@gmail.com>
2026-04-14 16:11:00 -0300

    fix: update Meta Pixel ID to 1344919667535954

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,13 +57,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1178462170941397');
+            fbq('init', '1344919667535954');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display:"none"}}
-            src="https://www.facebook.com/tr?id=1178462170941397&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1344919667535954&ev=PageView&noscript=1"
           />
         </noscript>
       </body>
